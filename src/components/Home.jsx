4d743cec8a7c15144826b6d7cd576dfb7eb31995
kaye garcia <kayemgarcia@gmail.com>
2022-04-18 16:28:35 -0500

    Test h1 in Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import About from "./About";
+import Projects from "./Projects";
+
+import { Outlet } from "react-router-dom";
+
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      <h1>Hi, I'm Kaye Garcia</h1>
+      {/* <About />
+      <Projects /> */}
+    </div>
+  )
+}
+
+export default Home;
