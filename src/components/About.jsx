1c7a03aef19ef32b2303eeff12d3f849b60d445d
kaye garcia <kayemgarcia@gmail.com>
2022-04-18 16:28:45 -0500

    Test about in Home

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function About() {
+  return (
+    <div>
+      <h1 id="about">ABOUT ME</h1>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+      <h2>testest</h2>
+    </div>
+  )
+}
+
+export default About;
