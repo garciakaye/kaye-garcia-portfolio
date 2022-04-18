fccb31c89fd891920e6f7ce39cbd31195509f0e0
kaye garcia <kayemgarcia@gmail.com>
2022-04-18 16:28:22 -0500

    Create Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import "../styles/Navbar.css"
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav id="navbar">
+      <>
+        <nav>
+          <Link className="my-name" to="/">kayegarcia</Link>
+        </nav>
+        <nav>
+          <a href="/about" onClick={() => window.location.replace("/#about")}>About </a>
+          <a href="/projects" onClick={() => window.location.replace("/#projects")}>Projects </a>
+
+          {/* <div onClick={() => window.location.replace("/#about")}> */}
+          {/* <span>go to about</span>
+          </div> */}
+          {/* <Link to="/projects">Projects</Link>
+          <Link to="/skills">Skills</Link>
+          <Link to="/contact">Contact</Link> */}
+        </nav>
+      </>
+    </nav>
+
   )
 }
 
-export default Navbar
+export default Navbar;
